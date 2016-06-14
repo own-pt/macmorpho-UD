426977c01a79aaa36de9b7fc65f8ea56372585a9
Alexandre Rademaker <arademaker@gmail.com>
2016-06-14 19:01:08 -0300

    rules for keep pcp, new conllu files with pcp tag

--- process.lisp
+++ process.lisp
@@ -1,24 +1,33 @@
-(ql:quickload :cl-pcp)
-(in-package :string-rewriting)
+(ql:quickload :cl-tag-rewriting)
+(in-package :cl-tag-rewriting)
 
 (format t "Rodando...")
 (terpri)
 
-(defun process-macmorpho (rule-file)
+(defun process-macmorpho (rule-file log-file)
   (let* ((rules   (read-rules rule-file))
 	 (grammar (compile-rules rules)))
     (save-grammar-doc rules "grammar.text")
-    (process-file grammar "macmorpho-v1-train.txt" "macmorpho-v1-train-UD.txt")
-    (process-file grammar "macmorpho-v1-test.txt"  "macmorpho-v1-test-UD.txt")
-    (process-file grammar "macmorpho-v1-dev.txt"   "macmorpho-v1-dev-UD.txt")))
+    (process-file grammar "macmorpho-v1-train.txt" "macmorpho-v1-train-UD.txt" log-file)
+    (process-file grammar "macmorpho-v1-test.txt"  "macmorpho-v1-test-UD.txt" log-file)
+    (process-file grammar "macmorpho-v1-dev.txt"   "macmorpho-v1-dev-UD.txt" log-file)))
 
-(process-macmorpho "ud-remove-pcp.lisp")
+(process-macmorpho "ud-remove-pcp.lisp" "out-1.log")
 
-(convert-conllu "macmorpho-v1-train-UD.txt" "macmorpho-v1-train-UD.conllu")
-(convert-conllu "macmorpho-v1-test-UD.txt"  "macmorpho-v1-test-UD.conllu")
-(convert-conllu "macmorpho-v1-dev-UD.txt"   "macmorpho-v1-dev-UD.conllu")
+(convert-conllu "macmorpho-v1-train-UD.txt" "macmorpho-v1-train.conllu")
+(convert-conllu "macmorpho-v1-test-UD.txt"  "macmorpho-v1-test.conllu")
+(convert-conllu "macmorpho-v1-dev-UD.txt"   "macmorpho-v1-dev.conllu")
 
-(tabulate-log "out.log" "out.tab")
-(rules-not-used "ud-remove-pcp.lisp" "out.tab")
+(tabulate-log "out-1.log" "out-1.tab")
+(print (rules-not-used "ud-remove-pcp.lisp" "out-1.tab"))
+
+(process-macmorpho "ud-keep-pcp.lisp" "out-2.log")
+
+(convert-conllu "macmorpho-v1-train-UD.txt" "macmorpho-v1-train-pcp.conllu")
+(convert-conllu "macmorpho-v1-test-UD.txt"  "macmorpho-v1-test-pcp.conllu")
+(convert-conllu "macmorpho-v1-dev-UD.txt"   "macmorpho-v1-dev-pcp.conllu")
+
+(tabulate-log "out-2.log" "out-2.tab")
+(print (rules-not-used "ud-keep-pcp.lisp" "out-2.tab"))
 
 (sb-ext:quit)
