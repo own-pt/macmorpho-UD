13fbc38aa609c97cf5c4c7f84268511d9eda3803
Alexandre Rademaker <arademaker@gmail.com>
2016-08-19 17:27:34 -0300

    last run new process.lisp

--- process.lisp
+++ process.lisp
@@ -4,32 +4,21 @@
 (format t "Rodando...")
 (terpri)
 
-(defun process-macmorpho (rule-file log-file)
-  (let* ((rules   (read-rules rule-file))
-	 (grammar (compile-rules rules)))
-    (save-grammar-doc rules "grammar.text")
-    (process-file grammar "macmorpho-v1-train.txt" "macmorpho-v1-train-UD.txt" log-file)
-    (process-file grammar "macmorpho-v1-test.txt"  "macmorpho-v1-test-UD.txt" log-file)
-    (process-file grammar "macmorpho-v1-dev.txt"   "macmorpho-v1-dev-UD.txt" log-file)))
+(defun process-macmorpho (rule-file suffix log-file)
+  (let ((grammar (compile-rules (read-rules rule-file)))
+	(files   '("train" "test" "dev")))
+    (dolist (file files)
+      (let ((in   (format nil "macmorpho-v1-~a.txt" file))
+	    (out1 (format nil "macmorpho-v1-~a-~a.tmp" file suffix))
+	    (out2 (format nil "macmorpho-v1-~a-~a.conllu" file suffix)))
+	(process-file grammar in out1 log-file)
+	(convert-conllu out1 out2)))))
 
-(process-macmorpho "ud-remove-pcp.lisp" "out-1.log")
+(process-macmorpho "ud-remove-pcp.lisp" "remove-pcp" "ud-remove-pcp.log")
+(process-macmorpho "ud-keep-pcp.lisp" "keep-pcp" "ud-keep-pcp.log")
 
-(convert-conllu "macmorpho-v1-train-UD.txt" "macmorpho-v1-train.conllu")
-(convert-conllu "macmorpho-v1-test-UD.txt"  "macmorpho-v1-test.conllu")
-(convert-conllu "macmorpho-v1-dev-UD.txt"   "macmorpho-v1-dev.conllu")
-
-(tabulate-log "out-1.log" "out-1.tab")
-(format *standard-output* "not used in ud-remove-pcp : ~% ~{~a~%~}~%"
-	(rules-not-used "ud-remove-pcp.lisp" "out-1.tab"))
-
-(process-macmorpho "ud-keep-pcp.lisp" "out-2.log")
-
-(convert-conllu "macmorpho-v1-train-UD.txt" "macmorpho-v1-train-pcp.conllu")
-(convert-conllu "macmorpho-v1-test-UD.txt"  "macmorpho-v1-test-pcp.conllu")
-(convert-conllu "macmorpho-v1-dev-UD.txt"   "macmorpho-v1-dev-pcp.conllu")
-
-(tabulate-log "out-2.log" "out-2.tab")
-(format *standard-output* "not used in ud-keep-pcp :~% ~{~a~%~}~%"
-	(rules-not-used "ud-keep-pcp.lisp" "out-2.tab"))
+;; (tabulate-log "out-2.log" "out-2.tab")
+;; (format *standard-output* "not used in ud-keep-pcp :~% ~{~a~%~}~%"
+;; 	(rules-not-used "ud-keep-pcp.lisp" "out-2.tab"))
 
 (sb-ext:quit)
