3d5fb7460f61006ea50e99aff1874f4fb8c53c34
Guilherme Paulino Passos <gpaulino@br.ibm.com>
2017-09-30 01:22:56 -0300

    Fixing, same command for any dataset model

--- freeling-training/run_analyzer.sh
+++ freeling-training/run_analyzer.sh
@@ -1,17 +1,17 @@
 INPUTFILE=$1
+DATASET=$2
 
 # receives file as conllu
 # outputs as conllu
 
-DATA=$(mktemp)
-awk '{print $2} END {print ""}' $INPUTFILE > $DATA
+INPUT=$(mktemp)
+awk '{print $2} END {print ""}' $INPUTFILE > $INPUT
 
 OUTPUT=$(mktemp)
-/usr/local/bin/analyzer -f analyzer.cfg <$DATA > $OUTPUT
+/usr/local/bin/analyzer -f analyzer_$DATASET.cfg <$INPUT > $OUTPUT
 
 awk -v 'OFS=\t' 'BEGINFILE {id=1} $0 == "" {id=1; print $0} $0 != ""  {print id++, $1, $2, $3, "_", "_", "0", "_", "_", "_" ; id++} ' $OUTPUT
 
-# rm $DATA
-# rm $OUTPUT
-echo $DATA
-echo $OUTPUT
+rm $INPUT
+rm $OUTPUT
+
