925e859a959476f07193666a3d7355f8422f4856
Guilherme Paulino Passos <gpaulino@br.ibm.com>
2017-10-23 17:38:17 -0200

    Changed run_analyzer.sh

--- freeling-training/run_analyzer.sh
+++ freeling-training/run_analyzer.sh
@@ -5,12 +5,12 @@ DATASET=$2
 # outputs as conllu
 
 INPUT=$(mktemp)
-awk '{print $2} END {print ""}' $INPUTFILE > $INPUT
+awk '$1 ~ /^[0-9]+$/ {print $2} $0 == "" {print ""} END {print ""}' $INPUTFILE > $INPUT
 
 OUTPUT=$(mktemp)
 /usr/local/bin/analyzer -f analyzer_$DATASET.cfg <$INPUT > $OUTPUT
 
-awk -v 'OFS=\t' 'BEGINFILE {id=1} $0 == "" {id=1; print $0} $0 != ""  {print id++, $1, $2, $3, "_", "_", "0", "_", "_", "_" ; id++} ' $OUTPUT
+awk -v 'OFS=\t' 'BEGINFILE {id=1} $0 == "" {id=1; print $0} $0 != ""  {print id, $1, $2, $3, "_", "_", "0", "_", "_", "_" ; id++} ' $OUTPUT
 
 rm $INPUT
 rm $OUTPUT
