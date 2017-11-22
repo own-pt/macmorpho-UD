18710d3496828ffa3504f4ac09657168912ed1fb
Guilherme Paulino Passos <gpaulino@br.ibm.com>
2017-11-22 17:12:40 -0200

    deleted unused run.sh script

--- opennlp/train-and-eval/run.sh
+++
@@ -1,8 +0,0 @@
-for f in {keep-pcp,remove-pcp,mm-revisto}; do
-    if [ ! -e pt-$f ]; then
-	~/apache-opennlp-1.8.2/bin/opennlp POSTaggerTrainer.conllu -encoding utf-8 -lang pt -model pt-$f -data ../macmorpho-v1-train-$f.conllu > pt-$f.train.log
-    else
-	echo skipping $f
-	~/apache-opennlp-1.8.2/bin/opennlp POSTaggerEvaluator.conllu -model pt-$f -data ../macmorpho-v1-test-$f.conllu -encoding utf-8 > pt-$f.eval.log
-    fi
-done
